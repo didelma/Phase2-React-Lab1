f042d633909d86bf6d3426ae463552cc6caba93a
Jenny <jennyd.moncion@gmail.com>
2022-01-04 23:13:59 -0500

    project init

---
+++ src/component/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  return (
+    <nav>
+      <div>
+          <h2>Book</h2>
+      </div>
+      <div>
+        <li>Home</li>
+        <li>Books</li>
+        <li>About</li>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
